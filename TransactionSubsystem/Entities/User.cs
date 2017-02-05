a7d115f319a0d1382fc6f97b30aabc8492fc7cfe
Dmitry <Dmitry@EXTREEM-PC>
2017-02-05 21:55:01 +0500

    User entity now is not anemic model

--- TransactionSubsystem/Entities/User.cs
+++ TransactionSubsystem/Entities/User.cs
@@ -11,32 +11,61 @@ namespace TransactionSubsystem.Entities
         }
         public User(string name, string email)
         {
-            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException((name));
-            if (string.IsNullOrWhiteSpace(email)) throw new ArgumentException((email));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("name");
+            if (string.IsNullOrWhiteSpace(email)) throw new ArgumentException("email");
 
             Name = name;
             Email = email;
+            CurrentBalance = 500;
         }
 
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Salt { get; set; }
-        public string HashedPassword { get; set; }
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Salt { get; protected set; }
+        public string HashedPassword { get; protected set; }
+
+        //[NotMapped]
+        //public Transaction PreparingTransaction { get; set; }
+
+        public decimal CurrentBalance { get; protected set; }
 
         [NotMapped]
-        public Transaction PreparingTransaction { get; set; }
+        public IEnumerable<Transaction> Transactions { get; protected set; }
 
-        public decimal CurrentBalance { get; set; }
+        //public void PrepareNewTransaction()
+        //{
+        //    PreparingTransaction = new Transaction();
+        //    PreparingTransaction.TransactionOwner = this;
+        //    // todo
+        //}
 
-        [NotMapped]
-        public IEnumerable<Transaction> CommitedTransactions { get; set; }
-
-        public void PrepareNewTransaction()
+        public void ExecuteTransaction(Transaction transaction)
         {
-            PreparingTransaction = new Transaction();
-            PreparingTransaction.TransactionOwner = this;
+            //todo
+            if (transaction == null) throw new ArgumentNullException("Transaction");
+            if (transaction.Amount <= 0) throw new ArgumentException("Transaction amount must be greater than 0");
+           
             // todo
+            transaction.TransactionOwner = this;
+
+            if (transaction.TransactionOwner.Name == transaction.Recepient.Name) throw new ArgumentException("Recipient must be different from the transaction sender");
+            if (transaction.Amount > transaction.TransactionOwner.CurrentBalance) throw new ArgumentException("Not enough money");
+
+            transaction.TransactionOwner.CurrentBalance -= transaction.Amount;
+            transaction.Recepient.CurrentBalance += transaction.Amount;
+
+            transaction.OwnerResultingBalance = transaction.TransactionOwner.CurrentBalance;
+            transaction.RecepientResultingBalance = transaction.Recepient.CurrentBalance;
+        }
+
+        public void FillSecurityProperties(string passwordSalt, string hashedPassword)
+        {
+            if (string.IsNullOrWhiteSpace(passwordSalt)) throw new ArgumentException("passwordSalt");
+            if (string.IsNullOrWhiteSpace(hashedPassword)) throw new ArgumentException("hashedPassword");
+
+            Salt = passwordSalt;
+            HashedPassword = hashedPassword;
         }
     }
 }
