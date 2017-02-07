22cba6c88c9dc49802452c63f41f9f3e4127ffbe
Dmitry <Dmitry@EXTREEM-PC>
2017-02-07 22:07:08 +0500

    added custom auth filter

---
+++ ParrotWings/Filters/CustomAuthenticationAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using System.Web.Http.Results;
+using System.Security.Principal;
+
+namespace ParrotWings.Filters
+{
+    class CustomAuthenticationAttribute : Attribute, IAuthenticationFilter
+    {
+        public Task AuthenticateAsync(HttpAuthenticationContext context,
+            CancellationToken cancellationToken)
+        {
+            context.Principal = null;
+            AuthenticationHeaderValue authentication = context.Request.Headers.Authorization;
+            if (authentication != null && authentication.Scheme == "Basic")
+            {
+                string login = Encoding.ASCII.GetString(Convert.FromBase64String(authentication.Parameter));
+                string[] roles = new string[] {"user"};
+                
+                context.Principal = new GenericPrincipal(new GenericIdentity(login), roles);
+            }
+            if (context.Principal == null)
+            {
+                context.ErrorResult
+                    = new UnauthorizedResult(new AuthenticationHeaderValue[]
+                    {
+                        new AuthenticationHeaderValue("Basic")
+                    }, context.Request);
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context,
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+        }
+
+        public bool AllowMultiple
+        {
+            get { return false; }
+        }
+    }
+}
